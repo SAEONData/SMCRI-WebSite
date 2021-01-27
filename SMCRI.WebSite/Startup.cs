b1cce51d9b5cc44cf9a64baec8af90edada5cc43
Tim Parker-Nance (SAEON) <timpn@saeon.ac.za>
2021-01-27 16:38:38 +0200

    Clarity added

--- SMCRI.WebSite/Startup.cs
+++ SMCRI.WebSite/Startup.cs
@@ -19,6 +19,7 @@ namespace SMCRI.WebSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
