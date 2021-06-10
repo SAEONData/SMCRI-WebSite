d44d496bad7312460aaca09693aef136db6c7125
Tim Parker-Nance (SAEON) <timpn@saeon.ac.za>
2021-06-10 16:16:31 +0200

    MDB v5 upgrade, node, compression, caching added

---
+++ SMCRI.WebSite/Defaults.cs
@@ -0,0 +1,7 @@
+﻿namespace SMCRI.WebSite
+{
+    public static class Defaults
+    {
+        public const int CacheDuration = 60 * 60 * 24 * 7;
+    }
+}
--- SMCRI.WebSite/Startup.cs
+++ SMCRI.WebSite/Startup.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Linq;
 
 namespace SMCRI.WebSite
 {
@@ -18,8 +23,32 @@ namespace SMCRI.WebSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    options.Secure = CookieSecurePolicy.Always;
+            //    options.HttpOnly = HttpOnlyPolicy.Always;
+            //    options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+            //});
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.None;
+            });
+            services.AddResponseCaching();
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "image/jpg", "image/jpeg", "image/png", "application/pdf" });
+            });
+
             services.AddControllersWithViews();
             services.AddApplicationInsightsTelemetry();
+            IFileProvider physicalProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+            services.AddSingleton<IFileProvider>(physicalProvider);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,10 +65,25 @@ namespace SMCRI.WebSite
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseResponseCompression();
+            app.UseResponseCaching();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.CacheControl] = $"public,max-age={Defaults.CacheDuration}";
+                }
+            });
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+                RequestPath = "/node_modules",
+            });
+
 
             app.UseRouting();
 
+            app.UseCookiePolicy();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
